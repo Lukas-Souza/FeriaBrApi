e897c4c7ed66bf92e566fa92ce8976f034d471f8
Lucas Henrique <lucashenriquedelimasouza2006@gmail.com>
2025-11-08 16:44:57 -0300

    ADD CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = require("./Router/router")
+const cors = require("cors");
 const app = express()
 const PORT = 8000
+app.use(cors())
 app.use(router)
+
 app.use(express.json());
 module.exports = app 
     
